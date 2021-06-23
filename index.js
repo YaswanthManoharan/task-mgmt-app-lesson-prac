f130969939833a6f8f1453279ed61fbea71e0b79
YaswanthManoharan <yaswanthtarun1@gmail.com>
2021-06-23 19:31:47 +0530

    upto Localstorage

---
+++ index.js
@@ -0,0 +1,66 @@
+const taskContainer = document.querySelector(".task__container");
+
+const globalStore=[];
+const generateNewCard = (taskData) => `<div class="col-md-6 col-lg-4" id=${taskData.id}>
+<div class="card">
+  <div class="card-header d-flex justify-content-end gap-2">
+    <button type="button" class="btn btn-outline-success">
+      <i class="fas fa-pencil-alt"></i>
+    </button>
+    <button type="button" class="btn btn-outline-danger">
+      <i class="fas fa-trash-alt"></i>
+    </button>
+  </div>
+  <img
+    src=${taskData.imageUrl}
+    class="card-img-top"
+    alt="..."
+  />
+  <div class="card-body">
+    <h5 class="card-title">${taskData.taskTitle}</h5>
+    <p class="card-text">
+      ${taskData.taskDescription}
+    </p>
+    <a href="#" class="btn btn-primary">${taskData.taskType}</a>
+  </div>
+  <div class="card-footer">
+    <button type="button" class="btn btn-outline-primary float-end">
+      Open Task
+    </button>
+  </div>
+</div>
+</div>`;
+
+const loadInitialCardData = ()=>{
+  //localstorage to get tasky data
+  const getCardData = localStorage.getItem("tasky");
+
+  //Convert string to JS normal object
+  const {cards} = JSON.parse(getCardData);
+
+  //loop over the array of task object to create html card and inject to DOM
+  cards.map((cardObject)=>{taskContainer.insertAdjacentHTML("beforeend", generateNewCard(cardObject));
+  
+  //Update our globalStore
+  globalStore.push(cardObject);
+})
+
+  
+};
+
+const saveChanges = () => {
+  const taskData = {
+    id: `${Date.now()}`, // unique number for id
+    imageUrl: document.getElementById("imageurl").value,
+    taskTitle: document.getElementById("tasktitle").value,
+    taskType: document.getElementById("tasktype").value,
+    taskDescription: document.getElementById("taskdescription").value,
+  };
+
+  taskContainer.insertAdjacentHTML("beforeend", generateNewCard(taskData));
+
+  globalStore.push(taskData);
+  localStorage.setItem("tasky",JSON.stringify({cards:globalStore}));
+};
+
+
